8e181f0dac97fd0a56a91506fc23535c6eea966c
Clos99 <36385304+Clos99@users.noreply.github.com>
2018-03-14 12:58:13 -0700

    Update BSTree.java

--- Binary Tree Experiment/src/BSTree.java
+++ Binary Tree Experiment/src/BSTree.java
@@ -1,18 +1,18 @@
 
 public class BSTree {
 	BSTNode root;
-	
+	//I complete forgot what i was doing here lmao
 	public BSTree()
 	{
 		root = null;
 	}
 	
 	public void insert(int n) {
-		root.insert(n);
+		root.insert(n);//this goes into the BSTNode 
 	}
 	
 	public void delete(int n) {
-		root.delete(n);
+		root.delete(n);//goes into BSTNode
 	}
 
 }
